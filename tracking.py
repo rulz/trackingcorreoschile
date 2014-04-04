501296b935517639657b3cfee90324edcdee8574
Rulz <raul@witoi.com>
2014-04-04 15:39:50 -0300

    mando un json final

--- tracking.py
+++ tracking.py
@@ -3,7 +3,7 @@ import json
 from BeautifulSoup import BeautifulSoup
 
 __author__ = 'Raul Sepulveda'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 def get_data_tracking(code):
 	dict = {}
@@ -16,9 +16,9 @@ def get_data_tracking(code):
 	for i,row in enumerate(table):
 		if i==1: 
 			dict['code'] = code
-			dict['State'] = str(row.findAll('td')[0]).replace('<td>&nbsp;&nbsp;','').replace('&nbsp;</td>','').replace("  ","")
-			dict['Date'] = str(row.findAll('td')[1]).replace('<td align="center">','').replace('&nbsp;</td>','').replace("  ","")
-			dict['Oficina'] = str(row.findAll('td')[2]).replace('<td>&nbsp;&nbsp;','').replace('&nbsp;</td>','').replace("  ","")
+			dict['status'] = str(row.findAll('td')[0]).replace('<td>&nbsp;&nbsp;','').replace('&nbsp;</td>','').replace("  ","")
+			dict['date'] = str(row.findAll('td')[1]).replace('<td align="center">','').replace('&nbsp;</td>','').replace("  ","")
+			dict['office'] = str(row.findAll('td')[2]).replace('<td>&nbsp;&nbsp;','').replace('&nbsp;</td>','').replace("  ","")
 
 	return dict
 
@@ -27,5 +27,5 @@ def code_list(list_code):
 	for code in list_code:
 		response.append(get_data_tracking(code))
 	
-	return json.dumps(response)
+	return json.loads(json.dumps(response, ensure_ascii=False))
 
